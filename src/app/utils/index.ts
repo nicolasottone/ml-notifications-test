60a4ba32503885b3554a223eb7f70ae94316a769
Nicolas Ottone <nicolasottone@hotmail.com>
2023-12-02 00:33:23 -0300

    getQuestionsByItemAndUser function added

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -9,13 +9,36 @@ export async function getQuestionDetails(questionId: string) {
       }
     })
     if (!response.ok) {
-      console.error('Error fetching question details:', response.statusText)
+      console.error(`Error fetching question ${questionId} details:`, response.statusText)
       return null
     }
     const question: QuestionDetailsType = await response.json()
     return question.text
   } catch (error) {
-    console.error('Error fetching question details:', error)
+    console.error(`Error fetching question ${questionId} details:`, error)
+    return null
+  }
+}
+
+export async function getQuestionsByItemAndUser(itemId: string, userId: string) {
+  try {
+    const response = await fetch(
+      `https://api.mercadolibre.com/questions/search?item=${itemId}&from=${userId}&api_version=4`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`
+        }
+      }
+    )
+    if (!response.ok) {
+      console.error(`Error fetching questions from item: ${itemId} & user: ${userId}`, response.statusText)
+      return null
+    }
+    const question: QuestionDetailsType = await response.json()
+    return question.text
+  } catch (error) {
+    console.error(`Error fetching questions from item: ${itemId} & user: ${userId}`, error)
     return null
   }
 }

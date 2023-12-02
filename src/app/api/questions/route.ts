0b490d58af306376dccdc75db4c66cd218289f53
Nicolas Ottone <nicolasottone@hotmail.com>
2023-12-02 16:23:33 -0300

    Sample notification button added in UI

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-let variable: any = { status: 'Todavia no se recibio nada' }
+let variable: any = { status: 'Nothing to see' }
 
 export async function POST(req: Request) {
   variable = await req.json()
-  console.log('se ha redirigidio el POST', variable)
+  console.log('se ha redirigidio la sig noti', variable)
+  
   return NextResponse.json({})
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,24 @@
 import { useState } from 'react'
 
 export default function Home() {
-  const fetchPOST = async () => {
+  const sendNotificationTest = async () => {
     const res = await fetch('api/notifications', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ test: 'Este es el vendito test' })
+      body: JSON.stringify({
+        _id: 'f9f08571-1f65-4c46-9e0a-c0f43faas1557e',
+        resource: '/questions/5036111111',
+        user_id: 123456789,
+        topic: 'questions',
+        application_id: 2069392825111111,
+        attempts: 1,
+        sent: '2017-10-09T13:51:05.464Z',
+        received: '2017-10-09T13:51:05.438Z'
+      })
     })
   }
 
-  const fetchGET = async () => {
+  const getLastNotification = async () => {
     const res = await (
       await fetch('api/questions', {
         method: 'GET'
@@ -24,8 +33,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex gap-7 p-4">
-        <button onClick={fetchPOST}>POST</button>
-        <button onClick={fetchGET}>GET</button>
+        <button onClick={sendNotificationTest}>SEND NOTIFICATION TEST</button>
+        <button onClick={getLastNotification}>GET LAST NOTIFICATION</button>
       </div>
       <div className="max-w-5xl w-full flex-col gap-4 font-mono lg:flex">
         <h2>ULTIMA NOTI RECIBIDA</h2>

5688e5adad8a08c2960336f426ed56fd3ff442b9
Nicolas Ottone <nicolasottone@hotmail.com>
2023-12-02 01:48:26 -0300

    getUnansweredQuestionsByItem function added

--- src/app/utils/questions/index.ts
+++ src/app/utils/questions/index.ts
@@ -86,6 +86,35 @@ export async function getQuestionsByItem(itemId: string) {
 }
 
 /**
+ * Fetches unanswered questions associated with a specific item, sorted in ascending order by creation date.
+ * @param {string} itemId - The ID of the item for which questions are to be obtained.
+ * @returns {Promise<QuestionDetailsType[]|null>} - Unanswered questions array or null if there is an error.
+ * @throws {Error} - Throws an error if there is an issue making the API request.
+ */
+export async function getUnansweredQuestionsByItem(itemId: string) {
+  try {
+    const response = await fetch(
+      `https://api.mercadolibre.com/questions/search?item=${itemId}&status=UNANSWERED&sort_fields=date_created&sort_types=ASC&api_version=4`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${process.env.TOKEN}`
+        }
+      }
+    )
+    if (!response.ok) {
+      console.error(`Error fetching unanswered questions from item: ${itemId}`, response.statusText)
+      return null
+    }
+    const data: ApiQuestionResponse = await response.json()
+    return data.questions
+  } catch (error) {
+    console.error(`Error fetching unanswered questions from item: ${itemId}`, error)
+    return null
+  }
+}
+
+/**
  * Fetches questions associated with a specific seller, sorted in ascending order by creation date.
  * @param {string} sellerId - The ID of the seller for whom questions are to be obtained.
  * @returns {Promise<QuestionDetailsType[]|null>} - questions arrays or null if there is an error.
@@ -132,13 +161,13 @@ export async function getUnansweredQuestionsBySeller(sellerId: string) {
       }
     )
     if (!response.ok) {
-      console.error(`Error fetching questions from seller: ${sellerId}`, response.statusText)
+      console.error(`Error fetching unanswered questions from seller: ${sellerId}`, response.statusText)
       return null
     }
     const data: ApiQuestionResponse = await response.json()
     return data.questions
   } catch (error) {
-    console.error(`Error fetching questions from seller: ${sellerId}`, error)
+    console.error(`Error fetching unanswered questions from seller: ${sellerId}`, error)
     return null
   }
 }
